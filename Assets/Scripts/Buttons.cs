3eae8a3997083cfe00ba41511a723cc75b158310
Joseph Gil <josephg1999@gmail.com>
2025-07-26 20:20:11 -0400

    Defeat() function should be working now. Issue was a timing issue with the Destroy() function and the getters and setters from the Weapons and WeaponClass scripts.

--- Assets/Scripts/Buttons.cs
+++ Assets/Scripts/Buttons.cs
@@ -5,20 +5,19 @@ public class Buttons : MonoBehaviour
     public GameObject Target, Button, Prefab, WeaponClone;
     [SerializeField] GameManager Manager;
     [SerializeField] EnemySpawn EnemySpawn; [SerializeField] Enemies Enemy; //get EnemySpawn script and Enemy script so we can call their functions for weapon interaction
-    [SerializeField] Weapons Weapon;
+    [SerializeField] Weapons Weapon; [SerializeField] WeaponClass WeaponClass;
     [SerializeField] Transform Spawn; //get item spawn's position/rotation to use in Instantiate function
 
     public void weaponSelect() 
     {
         Debug.Log("weapon select");
-        WeaponClone = Instantiate(Prefab, Spawn.position, Spawn.rotation);
-        Manager.enterInventory(); //takes us back to battle screen
-        Destroy(Target);
-        if (Enemy.getWeakness1.ToString() == Weapon.getCondition || Enemy.getWeakness2.ToString() == Weapon.getCondition) 
+        Weapon.getCondition = WeaponClass.condition;
+        if (Enemy.getWeakness1.ToString() == Weapon.getCondition) 
         {
-            Debug.Log("Inside if statement");
+            Debug.Log("IT'S A MATCH");
             EnemySpawn.Defeat();
         }
-        //Destroy(WeaponClone);
+        WeaponClone = Instantiate(Prefab, Spawn.position, Spawn.rotation);
+        Manager.enterInventory(); //takes us back to battle screen
     }
 }
--- Assets/Scripts/WeaponClass.cs
+++ Assets/Scripts/WeaponClass.cs
@@ -3,16 +3,15 @@ using UnityEngine;
 public class WeaponClass : MonoBehaviour
 {
     [SerializeField] Weapons weaponStats;
-    [SerializeField] string weaponName, condition;
+    public string weaponName, condition;
     void Start()
     {
-        weaponStats.getTitle = weaponName;
-        weaponStats.getCondition = condition;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 }
